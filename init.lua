4a32bbf8557b480d74051376581e4712b3c7b19d
Bernhard Bauer <hello@bbauer.me>
2025-05-25 22:23:32 +0200

    add overseer config

--- init.lua
+++ init.lua
@@ -345,6 +345,7 @@ require('lazy').setup({
       -- Document existing key chains
       spec = {
         { '<leader>f', group = '[F]ind' },
+        { '<leader>r', group = '[R]un' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
@@ -937,6 +938,22 @@ require('lazy').setup({
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
+  },
+  {
+    'stevearc/overseer.nvim',
+    opts = {},
+    config = function()
+      require('overseer').setup()
+    end,
+    keys = {
+      { '<leader>rv', '<cmd>OverseerToggle<cr>', desc = '[R]un [V]iew' },
+      { '<leader>ra', '<cmd>OverseerRun<cr>', desc = '[R]un [A]ny' },
+      { '<leader>rt', '<cmd>OverseerTaskAction<cr>', desc = '[R]un [T]ask' },
+      { '<leader>rq', '<cmd>OverseerQuickAction<cr>', desc = '[R]un [Q]uick Action recent task' },
+      { '<leader>ri', '<cmd>OverseerInfo<cr>', desc = '[R]und [I]nfo' },
+      { '<leader>rb', '<cmd>OverseerBuild<cr>', desc = '[R]un [B]uilder' },
+      { '<leader>rc', '<cmd>OverseerClearCache<cr>', desc = '[R]un [C]lear cache' },
+    },
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
