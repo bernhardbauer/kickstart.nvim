e14b31f53c2dc42810ad7e436444c26c4d8a59d3
Bernhard Bauer <hello@bbauer.me>
2025-05-29 20:07:30 +0200

    add code companion with local llm

--- init.lua
+++ init.lua
@@ -1053,6 +1053,47 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    'olimorris/codecompanion.nvim',
+    opts = {
+      adapters = {
+        opts = {
+          show_defaults = false,
+        },
+        qwen3 = function()
+          return require('codecompanion.adapters').extend('ollama', {
+            name = 'qwen3',
+            schema = {
+              model = {
+                default = 'qwen3:8b',
+              },
+              num_ctx = {
+                default = 16384,
+              },
+              num_predict = {
+                default = -1,
+              },
+            },
+          })
+        end,
+      },
+      strategies = {
+        chat = {
+          adapter = 'qwen3',
+        },
+        inline = {
+          adapter = 'qwen3',
+        },
+        cmd = {
+          adapter = 'qwen3',
+        },
+      },
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
