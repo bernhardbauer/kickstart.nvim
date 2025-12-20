e036438ee9d753f67a75b2400f598c12f15aa87d
Bernhard Bauer <hello@bbauer.me>
2025-12-20 14:44:36 +0100

    prevent opening buffers in overser windows, prevent overwriting window navigation by overseer

--- lua/custom/plugins/overseer.lua
+++ lua/custom/plugins/overseer.lua
@@ -5,7 +5,27 @@ return {
     config = function()
       require('overseer').setup {
         templates = { 'builtin', 'user.dotnet_run' },
+        task_list = {
+          bindings = {
+            ['<C-h>'] = false,
+            ['<C-j>'] = false,
+            ['<C-k>'] = false,
+            ['<C-l>'] = false,
+          },
+        },
       }
+
+      -- Make Overseer windows non-editable and prevent buffer switching
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'OverseerList',
+        callback = function()
+          vim.bo.modifiable = false
+          vim.bo.readonly = true
+          vim.bo.buftype = 'nofile'
+          -- Prevent switching buffers in this window (Neovim 0.10+)
+          vim.wo.winfixbuf = true
+        end,
+      })
     end,
     keys = {
       { '<leader>rv', '<cmd>OverseerToggle<cr>', desc = '[R]un [V]iew' },
