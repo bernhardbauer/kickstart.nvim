dcc7f512c24340452d9e87d9d08384d3fdee60f1
Bernhard Bauer <hello@bbauer.me>
2026-01-02 18:10:33 +0100

    add git diff view

---
+++ lua/custom/plugins/git.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'sindrets/diffview.nvim',
+    keys = {
+      { '<leader>hv', '<cmd>DiffviewOpen<cr>', desc = '[V]iew diff on index' },
+      { '<leader>hx', '<cmd>DiffviewClose<cr>', desc = '[X] Close diff view' },
+    },
+  },
+}
