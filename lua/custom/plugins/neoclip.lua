2a9edb59e0848b9e897dd6275b9d7b27e34c7817
Bernhard Bauer <hello@bbauer.me>
2026-01-05 20:27:22 +0100

    add clipboard history via telescrope

---
+++ lua/custom/plugins/neoclip.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'AckslD/nvim-neoclip.lua',
+    dependencies = {
+      { 'nvim-telescope/telescope.nvim' },
+    },
+    config = function()
+      require('neoclip').setup()
+    end,
+    keys = {
+      { '<leader>fc', '<cmd>Telescope neoclip<cr>', desc = '[C]lipboard history' },
+    },
+  },
+}
