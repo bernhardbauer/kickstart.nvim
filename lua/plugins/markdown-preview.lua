4070e1cb60c637a1034b83167ee031e583729332
Bernhard Bauer <hello@bbauer.me>
2026-03-05 21:38:37 +0100

    feat: fix markdown preview updates

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -3,7 +3,7 @@ return {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     ft = { 'markdown' },
-    build = 'cd app && npm install',
+    build = 'cd app && npm install && git checkout yarn.lock',
     init = function()
       vim.g.mkdp_filetypes = { 'markdown' }
     end,
