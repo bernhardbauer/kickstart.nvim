ed52cf50f8d7c43077c5459d3c4eba31d9cf3781
Bernhard Bauer <hello@bbauer.me>
2026-03-07 21:58:23 +0100

    fix: close all panels

--- lua/configs/nvim-close-all.lua
+++ lua/configs/nvim-close-all.lua
@@ -4,12 +4,13 @@ local M = {}
 
 -- Close all non-DAP panels (used when opening DAP UI)
 function M.close_non_dap_panels()
-  local explorer = Snacks.picker.get { source = 'explorer' }[1]
+  local explorer = Snacks.picker.get({ source = 'explorer' })[1]
   if explorer then
     explorer:close()
   end
   require('overseer').close()
   require('neotest').summary.close()
+  require('neotest').output_panel.close()
 end
 
 -- Close all plugin open panels
